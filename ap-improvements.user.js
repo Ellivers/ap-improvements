5dba570c8ea6e0ffd4b9c628949f8b6ea5f4a1ff
Ellivers <53975650+Ellivers@users.noreply.github.com>
2026-08-16 21:28:17 +0200

    Fixed customlink issue

--- ap-improvements.user.js
+++ ap-improvements.user.js
@@ -4672,7 +4672,7 @@ if (window.location.pathname.startsWith('/customlink')) {
           else iinfo.name = decodeURIComponent(value);
           const data = await getAnimeData(iinfo,["session","name"],{allowGuessing:true});
           if (!data.session) return;
-          if (iinfo.name && iinfo.name !== data.name && !confirm(`[AnimePahe Improvements]\n\nCouldn't find any anime with name "${name}".\nGo to "${animeData.title}" instead?`)) {
+          if (iinfo.name && iinfo.name !== data.name && !confirm(`[AnimePahe Improvements]\n\nCouldn't find any anime with name "${iinfo.name}".\nGo to "${data.name}" instead?`)) {
             return;
           }
           parts.animeSession = data.session;
