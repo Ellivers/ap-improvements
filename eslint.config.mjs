78ab3e33fa7a73866acd8770c67053cd87c357cf
Ellivers <53975650+Ellivers@users.noreply.github.com>
2026-08-23 17:29:57 +0200

    Added ESLint config to repo

---
+++ eslint.config.mjs
@@ -0,0 +1,21 @@
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+	{
+		files: ["**/*.js"],
+		plugins: { js },
+		extends: ["js/recommended"],
+		languageOptions: {
+			sourceType: "script",
+			globals: globals.browser,
+			parserOptions: {
+				ecmaFeatures: {
+					jsx: true,
+					globalReturn: true,
+				},
+			},
+		},
+	},
+]);
