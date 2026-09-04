e30b6245c73c343962f117fba61e37b6c1522e56
Ellivers <53975650+Ellivers@users.noreply.github.com>
2026-09-04 22:12:35 +0200

    Bumped version

--- ap-improvements.user.js
+++ ap-improvements.user.js
@@ -13,7 +13,7 @@
 // @grant       GM_setValue
 // @grant       GM_xmlhttpRequest
 // @grant       GM_info
-// @version     4.12.1
+// @version     4.13.0
 // @author      Ellivers
 // @license     MIT
 // @description Improvements and additions for the AnimePahe site
